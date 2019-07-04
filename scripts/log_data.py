b25e190cebed4aeff580209b2d6f91637a50defa
pPatrickK <patrick.klokowski@udo.edu>
2019-07-04 16:01:50 +0200

    Logging multiple log topics at once

--- scripts/log_data.py
+++ scripts/log_data.py
@@ -21,22 +21,30 @@ from crazyflie_driver.msg import GenericLogData
 CrazyswarmDir = os.path.dirname(os.path.realpath(__file__)) + "/../../crazyswarm"
 LogDir = os.path.dirname(os.path.realpath(__file__)) + "/../data"
 LogFiles = []
+N = 0 # number of topics
 now = datetime.datetime.now()
 
 def mf_callbacks(*collection):
 	k = 0
+	n = 0
+	row = []
 	for data in collection:
 		# rospy.loginfo(rospy.get_caller_id() + ": I got %s", data)
 		# rospy.loginfo(LogFiles[k])
 		# rospy.loginfo(str(rospy.get_param('/crazyswarm_server/genericLogTopic_log1_Variables')))
 
+		row.extend([data.header.stamp.secs + data.header.stamp.nsecs/1e9])
+		row.extend(data.values)
+		n = n + 1
+
 		# write values in csv
-		with open(LogFiles[k], 'aw') as csvFile:
-			writer = csv.writer(csvFile)
-			row = [data.header.stamp.secs + data.header.stamp.nsecs/1e9]
-			row.extend(data.values)
-			writer.writerow(row)
-		k = k + 1
+		if n%N == 0 and k < len(LogFiles):
+			with open(LogFiles[k], 'aw') as csvFile:
+				writer = csv.writer(csvFile)
+				writer.writerow(row)
+				rospy.loginfo(row)
+			row = []
+			k = k + 1
 
 if __name__ == '__main__':
 
@@ -45,12 +53,15 @@ if __name__ == '__main__':
 	subscriber = []
 	with open(CrazyswarmDir+"/launch/crazyflies.yaml") as stream:
 		allCFs = yaml.load(stream, yaml.FullLoader)
+		topics = rospy.get_param('/crazyswarm_server/genericLogTopics')
+		N = len(topics)
 		for cf in allCFs['crazyflies']:
 			id = cf['id']
 			file = LogDir+"/log_"+now.strftime("%Y_%m_%d_%H_%M")+"/CF"+str(cf['id'])+'.csv'
 			ids.append(id)
 			LogFiles.append(file)
-			subscriber.append(message_filters.Subscriber('/cf'+str(id)+'/log1', GenericLogData))
+			for top in topics:
+				subscriber.append(message_filters.Subscriber('/cf'+str(id)+'/'+top, GenericLogData))
 			# Check if dir exists and create CSV file with header:
 			if not os.path.exists(os.path.dirname(file)):
 			    try:
@@ -60,13 +71,17 @@ if __name__ == '__main__':
 			            raise
 			with open(file, 'w+') as csvFile:
 				writer = csv.writer(csvFile)
-				row = ["Time [s]"]
-				row.extend(rospy.get_param('/crazyswarm_server/genericLogTopic_log1_Variables'))
+				row = []
+				for top in topics:
+					row.extend(["Time [s]"])
+					row.extend(rospy.get_param('/crazyswarm_server/genericLogTopic_'+top+'_Variables'))
 				writer.writerow(row)
 
 	rospy.init_node('log_data', anonymous=False)
 	# rospy.Subscriber(ROS_TOPIC, GenericLogData, ros_callback)
-	ts = message_filters.ApproximateTimeSynchronizer(subscriber, 10, 0.01, allow_headerless=False)
-	# ts = message_filters.TimeSynchronizer([cf30, cf31], 15)
+	dt = rospy.get_param('/crazyswarm_server/genericLogTopicFrequencies') # ms
+	dt = dt[0] * 1e-3 # s
+	ts = message_filters.ApproximateTimeSynchronizer(subscriber, 10, 2.0/dt, allow_headerless=False)
+	# ts = message_filters.TimeSynchronizer(subscriber, 15)
 	ts.registerCallback(mf_callbacks)
 	rospy.spin()
